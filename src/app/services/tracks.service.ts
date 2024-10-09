b07eaf6ab08b9cc75a4afa1f9facafc3f8978290
nitesw <apedos2007@gmail.com>
2024-10-09 08:03:21 +0200

    Adds work with an api

--- src/app/services/tracks.service.ts
+++ src/app/services/tracks.service.ts
@@ -4,18 +4,22 @@ import { HttpClient } from '@angular/common/http';
 import { ITrack } from '../tracks/track';
 
 const api = 'https://localhost:7015/api/';
-const musicController = 'Music/';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TracksService {
+  controller: string = api + 'music/';
+
   constructor(private httpClient: HttpClient) {}
 
   getAll(): Observable<ITrack[]> {
-    return this.httpClient.get<ITrack[]>(api + musicController + 'all');
+    return this.httpClient.get<ITrack[]>(this.controller + 'all');
   }
-  deleteTrack(id: number): void {
-    this.httpClient.delete<number>(api + musicController + 'delete/' + id);
+  getId(id: number): Observable<ITrack> {
+    return this.httpClient.get<ITrack>(this.controller + 'getTrack?id=' + id);
+  }
+  deleteTrack(id: number): Observable<any> {
+    return this.httpClient.delete(this.controller + 'delete?id=' + id);
   }
 }
--- src/app/tracks/track-list/track-list.component.ts
+++ src/app/tracks/track-list/track-list.component.ts
@@ -1,26 +1,116 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  model,
+  ViewChild,
+} from '@angular/core';
 import { ITrack } from '../track';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { TracksService } from '../../services/tracks.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialog,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+export interface DialogData {
+  trackTitle: string;
+  trackId: number;
+}
 
 @Component({
   selector: 'app-track-list',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatIconModule, MatButtonModule, MatPaginatorModule],
   templateUrl: './track-list.component.html',
   styleUrl: './track-list.component.css',
 })
-export class TrackListComponent {
-  displayedColumns: string[] = ['id', 'title', 'isPublic', 'genreId'];
-  dataSource: ITrack[] = [];
+export class TrackListComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'id',
+    'title',
+    'isPublic',
+    'genreName',
+    'actions',
+  ];
+  dataSource = new MatTableDataSource<ITrack>([]);
 
-  constructor(private tracksService: TracksService) {
+  constructor(
+    private tracksService: TracksService,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {
     this.tracksService.getAll().subscribe((data) => {
-      this.dataSource = data;
+      this.dataSource.data = data;
     });
   }
 
   deleteTrack(id: number) {
     this.tracksService.deleteTrack(id);
   }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  openDeleteDialog(title: string, id: number) {
+    const dialogRef = this.dialog.open(ConfirmDialog, {
+      width: '250px',
+      enterAnimationDuration: '100ms',
+      exitAnimationDuration: '100ms',
+      data: {
+        trackTitle: title,
+        trackId: id,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((id) => {
+      if (id) {
+        this.tracksService.deleteTrack(id).subscribe(() => {
+          this.dataSource.data = this.dataSource.data.filter(
+            (x) => x.id !== id
+          );
+          this.openSnackBar();
+        });
+      }
+    });
+  }
+
+  openSnackBar() {
+    this.snackBar.open('Track deleted successfuly', 'Dismiss', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+}
+@Component({
+  selector: 'confirm-dialog',
+  templateUrl: './delete-confirm-dialog.html',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    MatPaginatorModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfirmDialog {
+  readonly dialogRef = inject(MatDialogRef<ConfirmDialog>);
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+  readonly id = model(this.data.trackId);
 }
